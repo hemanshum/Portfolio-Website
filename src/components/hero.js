f4fe191621aeaa49ccc83dcb00363afb30f0ec30
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-10 05:12:58 +0530

    Hero Section completed

--- src/components/hero.js
+++ src/components/hero.js
@@ -18,15 +18,27 @@ export default function Hero() {
     <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
       <div className='container mx-auto'>
         <div className='flex justify-between gap-x-8'>
-          <div>
-            <div>Web Developer</div>
-            <h1>Hello, my name is Ryan Dav</h1>
-            <p>
+          <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
+            <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'>
+              Web & Mobile App Developer
+            </div>
+            <h1 className='h1 mb-4'>Hi, my name is Hemanshu</h1>
+            <p className='subtitle max-w-[490px] mx-auto xl:mx-0'>
               Brief description with insigts into myself, my vocational journey,
               and what I engage in professional
             </p>
+            <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
+              <Link href='/contact'>
+                <Button className='gap-x-2'>
+                  Contact me <Send size={18} />
+                </Button>
+              </Link>
+              <Button variant='secondary' className='gap-x-2'>
+                Download CV <Download size={18} />
+              </Button>
+            </div>
           </div>
-          <div>image</div>
+          <div className='hidden xl:flex relative'>image</div>
         </div>
         <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
           <RiArrowDownSLine className='text-3xl text-primary' />
