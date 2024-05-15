535879e17f096340a64893bd020dbe708df3e6d3
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-15 20:09:39 +0530

    services section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from '@/components/hero';
 import About from '@/components/about';
+import Service from '@/components/services';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Service />
     </main>
   );
 }
