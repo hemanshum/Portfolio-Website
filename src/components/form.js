09b73d1ff4ec491baf1f36c87a7de954147dad44
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-17 11:47:45 +0530

    updated color

--- src/components/form.js
+++ src/components/form.js
@@ -9,15 +9,30 @@ export default function Form() {
   return (
     <form className='flex flex-col gap-y-4'>
       <div className='relative flex items-center'>
-        <Input type='name' id='name' placeholder='Name' />
+        <Input
+          type='name'
+          id='name'
+          placeholder='Name'
+          className='bg-[#32334C]'
+        />
         <User className='absolute right-6' size={20} />
       </div>
       <div className='relative flex items-center'>
-        <Input type='email' id='email' placeholder='Email' />
+        <Input
+          type='email'
+          id='email'
+          placeholder='Email'
+          className='bg-[#32334C]'
+        />
         <MailIcon className='absolute right-6' size={20} />
       </div>
       <div className='relative flex items-center'>
-        <Textarea type='name' id='name' placeholder='Type Your Message Here' />
+        <Textarea
+          type='name'
+          id='name'
+          placeholder='Type Your Message Here'
+          className='bg-[#32334C]'
+        />
         <MessageSquare className='absolute top-6 right-6' size={20} />
       </div>
       <Button className='flex items-center gap-x-2 max-w-[166px]'>
